be5f02eb845ac9d194b8a4a6c48381b5e29e1f0f
Nkaltaeva <n.kaltaeva@ya.ru>
2022-12-01 13:05:06 +0300

    React-7

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import {useSelector, useDispatch} from 'react-redux'
+import { useEffect, useState } from 'react';
+import { fetchItems, getPosts, testMiddleware } from './slices/item';
+
 
 function App() {
+
+  const items = useSelector(state => state.items)
+  const users = useSelector(state => state.items.users)
+  const dispatch = useDispatch()
+
+
+
+  useEffect(()=>{
+    dispatch(fetchItems())
+    console.log(items)
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>USERs</h1>
+      {
+        users.map(e => 
+        <div>
+          <h1>{e.name}</h1>
+          <p>{e.email}</p>
+        </div>
+        )
+      }
     </div>
   );
 }
 
 export default App;
+
